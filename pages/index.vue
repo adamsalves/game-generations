c810287d4bcb5d2e04bf4c1abf46154c19db4eb4
Adams Alves <alves.adams@gmail.com>
2021-10-08 10:14:30 -0300

    add static card component

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,14 @@
 <template>
-  <div></div>
+  <div class="columns">
+    <Card class="column" name="generation-i" />
+    <Card class="column" name="generation-ii" />
+    <Card class="column" name="generation-iii" />
+    <Card class="column" name="generation-iv" />
+    <Card class="column" name="generation-v" />
+    <Card class="column" name="generation-vi" />
+    <Card class="column" name="generation-vii" />
+    <Card class="column" name="generation-viii" />
+  </div>
 </template>
 
 <script>
@@ -18,3 +27,14 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.columns {
+  flex-flow: wrap;
+  margin-left: 0;
+  margin-right: 0;
+}
+.card {
+  margin: 35px;
+}
+</style>
